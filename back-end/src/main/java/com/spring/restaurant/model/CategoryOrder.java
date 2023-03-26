a63d0bdc1121588477b1530d6fa6b34a20b2a480
sman-ops <slimen.ghenimi@gmail.com>
2023-03-26 21:25:59 +0100

    add CreateTimestamp and UpdateTimestamp annotations

--- back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
@@ -1,7 +1,12 @@
 package com.spring.restaurant.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +20,14 @@ public class CategoryOrder extends BaseEntity {
 	
 	@Column(name="name")
 	private String name;
+	
+	@Column(name="data_create")
+	@CreationTimestamp
+	private Date dataCreate;
+	
+	@Column(name="data_update")
+	@UpdateTimestamp
+	private Date dataupdate;
 	
 	
 

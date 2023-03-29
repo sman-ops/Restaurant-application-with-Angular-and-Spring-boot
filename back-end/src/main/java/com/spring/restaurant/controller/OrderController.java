49db9cf3e7e22be4e908210df6f395b480e5b013
sman-ops <slimen.ghenimi@gmail.com>
2023-03-29 22:01:16 +0100

    show data of orders from spring and use @CrossOrigin

--- back-end/src/main/java/com/spring/restaurant/controller/OrderController.java
+++ back-end/src/main/java/com/spring/restaurant/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import com.spring.restaurant.model.Order;
 import com.spring.restaurant.service.OrderService;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class OrderController {
 	
 	private OrderService orderService;

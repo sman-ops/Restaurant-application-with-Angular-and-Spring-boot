570f302f6bbcaa78ab26d838883fd66091f5c2a8
sman-ops <slimen.ghenimi@gmail.com>
2023-03-31 01:01:52 +0100

    getOrders by categoryId and use of @RequestParam and @PathVariable

--- back-end/src/main/java/com/spring/restaurant/controller/OrderController.java
+++ back-end/src/main/java/com/spring/restaurant/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.restaurant.model.Order;
@@ -31,5 +32,23 @@ public class OrderController {
 		return new  ResponseEntity<>(orders,HttpStatus.OK);
 		
 	}
-
+	//http://localhost:8080/api/category?id={value}
+	@GetMapping("/api/category")
+	public ResponseEntity<List<Order>> getAllOrdersByCategoryId(@RequestParam Long id){
+		
+		List<Order> orders= orderService.getOrdersByIdCategories(id);
+		return new  ResponseEntity<>(orders,HttpStatus.OK);
+		
+	}
+	
+	//http://localhost:8080/api/category?id={value}
+	//	@GetMapping("/api/category/{id}")
+		//public ResponseEntity<List<Order>> getAllOrdersByCategoryId(@PathVariable Long id){
+			
+		//	List<Order> orders= orderService.getOrdersByIdCategories(id);
+			//return new  ResponseEntity<>(orders,HttpStatus.OK);
+			
+		//}
+	
+	
 }

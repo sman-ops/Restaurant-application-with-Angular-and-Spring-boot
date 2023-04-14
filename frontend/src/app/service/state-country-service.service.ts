9928b54f15d423bea024846b59980dcebd57c5f3
sman-ops <slimen.ghenimi@gmail.com>
2023-04-14 05:27:17 +0100

    add models in angular and retrieve data of all countries and state data from Spring in Angular service

---
+++ frontend/src/app/service/state-country-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Country } from '../model/country';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { State } from '../model/state';
+@Injectable({
+  providedIn: 'root',
+})
+export class StateCountryServiceService {
+  // ng g s state
+  private apiServerUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) {}
+
+  public getAllCountries(): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiServerUrl}/api/countries`)
+      .pipe(map((response: Country[]) => response));
+  }
+
+  public getAllStates(): Observable<State[]> {
+    return this.http
+      .get<State[]>(`${this.apiServerUrl}/api/states`)
+      .pipe(map((response: State[]) => response));
+  }
+}

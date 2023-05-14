269e8f9c07d6a573e50b35911eb72aa8d6bdb4f9
sman-ops <slimen.ghenimi@gmail.com>
2023-05-14 23:00:08 +0100

    angular login and signup build FormGroup

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  checkoutParentGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private formChildGroup: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myFormLogin();
   }
 
+  myFormLogin() {
+    this.checkoutParentGroup = this.formChildGroup.group({
+      user: this.formChildGroup.group({
+        email: [''],
+        password: [''],
+      }),
+    });
+  }
+
+  login() {
+    alert(this.checkoutParentGroup.controls['user'].value.email);
+  }
 }
--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  checkoutParentGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private formChildGroup: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myFormSignup();
   }
 
+  myFormSignup() {
+    this.checkoutParentGroup = this.formChildGroup.group({
+      user: this.formChildGroup.group({
+        email: [''],
+        password: [''],
+      }),
+    });
+  }
+
+  signup() {}
 }
